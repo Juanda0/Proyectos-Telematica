2b64a22e672638f520740c766d3dc43ab653d6cf
MrSossa <kasossac@gmail.com>
2022-10-24 19:36:59 -0500

    direction of connection changed

--- Proyecto 1/reverseProxy.py
+++ Proyecto 1/reverseProxy.py
@@ -7,19 +7,36 @@ import threading
 import config
 from utils import log
 printLock = threading.Lock()
-servers = []
+availableServers = []
 pos = 0
 
-def availableServer(msg):
-    global servers
-    servers.append([msg["id"],msg["ip_addr"],msg["listenport"]])
+def availableServer():
+    for server in config.SERVERS:
+        t = f"Connecting to the server with id {server[0]} on port {server[2]}\n"
+        log(t,file)
+
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.connect((server[1],server[2]))
+
+        jsonMsg = {"type":"1","listenport": config.RPPORT}
+        server_socket.send(json.dumps(jsonMsg).encode())
+        recvMsg = server_socket.recv(2048)
+        recvMsg = json.loads(recvMsg.decode())
+        if recvMsg["id"] == server[0]:
+            availableServers.append(server)
+            t = f"Conneted to the server with id {server[0]} on port {server[2]} succesful\n"
+            log(t,file)
+        else:
+            t = f"Connection to the server with id {server[0]} on port {server[2]} fail\n"
+            log(t,file)
+        server_socket.close()
 
 def roundRobin():
-    global servers
+    global availableServer
     global pos
-    host = servers[pos]
+    host = availableServers[pos]
     pos += 1
-    if pos >= len(servers):
+    if pos >= len(availableServers):
         pos = 0
     return host
 
@@ -32,53 +49,46 @@ def newClient(clientsocket,addr):
             break
         
         jsonMsg = json.loads(msg.decode())
-        if jsonMsg["type"] == "1":
-            ip, port = clientsocket.getpeername()
-            jsonMsg["ip_addr"] = ip
-            t = f"Received setup message from server id {jsonMsg['id']} ip {jsonMsg['ip_addr']} port {jsonMsg['listenport']} \n"
-            log(t,file)
-            
-            availableServer(jsonMsg)
-        elif jsonMsg["type"] == "0":
-            t = f"Received a message from client {jsonMsg['srcid']} payload {jsonMsg['payload']}\n"
-            log(t,file)
-            
-            
-            targetHost = roundRobin()
-            serverName = targetHost[1]
-            serverPort = int(targetHost[2])
+        
 
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_socket.connect((serverName,serverPort))
-
-            t = f"Forwarding a data message to server id {targetHost[0]} server ip  {serverName} port {serverPort} payload {jsonMsg['payload']} \n"
-            log(t,file)
+        t = f"Received a message from client {jsonMsg['srcid']} payload {jsonMsg['payload']}\n"
+        log(t,file)
             
-            server_socket.send(json.dumps(jsonMsg).encode())
-            recvMsg = server_socket.recv(2048)
-            recvMsg = json.loads(recvMsg.decode())
-            t = f"Received a data message from server id {recvMsg['srcid']} payload {recvMsg['payload']} \n"
-            log(t,file)
-            
-            server_socket.close()
+        targetHost = roundRobin()
+        serverName = targetHost[1]
+        serverPort = int(targetHost[2])
 
-            clientsocket.send(json.dumps(recvMsg).encode())
-        else:
-            pass
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.connect((serverName,serverPort))
+
+        t = f"Forwarding a data message to server id {targetHost[0]} server ip  {serverName} port {serverPort} payload {jsonMsg['payload']} \n"
+        log(t,file)
+            
+        server_socket.send(json.dumps(jsonMsg).encode())
+        recvMsg = server_socket.recv(2048)
+        recvMsg = json.loads(recvMsg.decode())
+        t = f"Received a data message from server id {recvMsg['srcid']} payload {recvMsg['payload']} \n"
+        log(t,file)
+            
+        server_socket.close()
+
+        clientsocket.send(json.dumps(recvMsg).encode())
+
+
     clientsocket.close()
 
 if __name__ == "__main__":
     global file
     file = f"logs/RP-{datetime.now().strftime('%d-%m-%Y-%H-%M-%S')}"
 
-    s = socket.socket()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     t = f"Running the reverse proxy on port {config.RPPORT}\n"
     log(t,file)
     
 
     s.bind((config.RPHOST, config.RPPORT))     
     s.listen(100)
-
+    availableServer()
     while True:
         c, addr = s.accept()
         printLock.acquire()
--- Proyecto 1/server.py
+++ Proyecto 1/server.py
@@ -9,8 +9,18 @@ from utils import log
 from datetime import date, datetime
 
 print_lock = threading.Lock()
+# Connect to the Reverse Proxy
+def rpConnection(rpSocket):
+    global ID,PORT
+    jsonMsg = {"type":"1", "id": ID, "listenport": PORT}
+    t = f"Connetion with reverse proxy\n"
+    log(t,file)
+    
+    rpSocket.send(json.dumps(jsonMsg).encode())
+
 # after receiving each connection from reverse proxy/client
 def newClient(clientsocket,addr):
+    global ID
     while True:
         msg = clientsocket.recv(2048)
         if not msg:
@@ -18,55 +28,44 @@ def newClient(clientsocket,addr):
             break
 
         jsonMsg = json.loads(msg.decode())
-        
-        t = f"Received a message from client {jsonMsg['srcid']} payload {jsonMsg['payload']}"
-        log(t,file)
+        if jsonMsg["type"] == "1":
+            rpConnection(clientsocket)
+        else:
+            t = f"Received a message from client {jsonMsg['srcid']} payload {jsonMsg['payload']}"
+            log(t,file)
 
-        payload = jsonMsg["payload"]
-        newMsg = hashlib.sha1()
-        newMsg.update(payload.encode())
-        hashedPayload = newMsg.hexdigest()
-        newJsonMsg = {"type":"2", "srcid": config.ID, "destid": jsonMsg["srcid"],"payloadsize": len(hashedPayload), "payload": hashedPayload}
-        
-        t = f"Sending a message to the client {newJsonMsg['destid']} payload {newJsonMsg['payload']}"
-        log(t,file)
+            payload = jsonMsg["payload"]
+            newMsg = hashlib.sha1()
+            newMsg.update(payload.encode())
+            hashedPayload = newMsg.hexdigest()
+            newJsonMsg = {"type":"2", "srcid": ID, "destid": jsonMsg["srcid"],"payloadsize": len(hashedPayload), "payload": hashedPayload}
+            
+            t = f"Sending a message to the client {newJsonMsg['destid']} payload {newJsonMsg['payload']}"
+            log(t,file)
 
-        clientsocket.send(json.dumps(newJsonMsg).encode())
+            clientsocket.send(json.dumps(newJsonMsg).encode())
     clientsocket.close()
-
-
-# Connect to the Reverse Proxy
-def rpConnection():
-    jsonMsg = {"type":"1", "id": config.ID, "listenport": config.PORT}
-    rpName = config.RPHOST
-    rpPort = config.RPPORT
-
-    rpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    rpSocket.connect((rpName, rpPort))
-
-    rpSocket.send(json.dumps(jsonMsg).encode())
-    rpSocket.close()
 
 if __name__ == "__main__":
     s = socket.socket()
-    global file
+    global file,ID,HOST,PORT
     file = f"logs/S-{datetime.now().strftime('%d-%m-%Y-%H-%M-%S')}"
+
+    ID = config.SERVERS[2][0]
+    HOST = config.SERVERS[2][1]
+    PORT = config.SERVERS[2][2]
     
-
-
-    
-
-    t = f"Server running with id {config.ID}\n"
+    t = f"Server running with id {ID}\n"
     log(t,file)
 
-    t = f"Listening on port {config.PORT}\n"
+    t = f"Listening on port {PORT}\n"
     log(t,file)
 
-    rpConnection()
-    t = f"Connecting to the reverse proxy on port {config.RPPORT}\n"
-    log(t,file)
+    # rpConnection()
+    # t = f"Connecting to the reverse proxy on port {config.RPPORT}\n"
+    # log(t,file)
 
-    s.bind((config.HOST, config.PORT))     
+    s.bind((HOST, PORT))     
     s.listen(10)                 
 
     while True:
